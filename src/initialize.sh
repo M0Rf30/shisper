c412146c459555e32cdabeccb4f4c7444bbb6254
M0Rf30 <morf3089@gmail.com>
2023-03-10 14:17:47 +0100

    chore: check for run-time dependencies

--- src/initialize.sh
+++ src/initialize.sh
@@ -4,3 +4,16 @@
 ## CONFIG_FILE=settings.ini
 ##
 ## Feel free to empty (but not delete) this file.
+required_executables=(whisper.cpp ffmpeg mpv)
+
+check_requirements() {
+  for executable in "${required_executables[@]}"; do
+    if ! command -v "${executable}" &>/dev/null; then
+      echo -e "[$(red_bold " FAILED ")] ${executable} executable is required"
+      exit 1
+    fi
+
+  done
+}
+
+check_requirements
